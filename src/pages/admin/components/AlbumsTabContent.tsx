cd0a7b66788edecb95b4aa952a9ddf81f0a7b918
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-11-14 18:09:24 +0700

    admin album

--- src/pages/admin/components/AlbumsTabContent.tsx
+++ src/pages/admin/components/AlbumsTabContent.tsx
@@ -1,3 +1,28 @@
-export default function AlbumsTabContent() {
-  return <div>AlbumsTabContent</div>;
-}
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Library } from "lucide-react";
+import AlbumsTable from "./AlbumsTable";
+import AddAlbumDialog from "./AddAdlbumDialog";
+
+const AlbumsTabContent = () => {
+	return (
+		<Card className='bg-zinc-800/50 border-zinc-700/50'>
+			<CardHeader>
+				<div className='flex items-center justify-between'>
+					<div>
+						<CardTitle className='flex items-center gap-2'>
+							<Library className='h-5 w-5 text-violet-500' />
+							Albums Library
+						</CardTitle>
+						<CardDescription>Manage your album collection</CardDescription>
+					</div>
+					<AddAlbumDialog />
+				</div>
+			</CardHeader>
+
+			<CardContent>
+				<AlbumsTable />
+			</CardContent>
+		</Card>
+	);
+};
+export default AlbumsTabContent;
